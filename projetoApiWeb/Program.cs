e755f14e69fe100ad7419476828b00e1bcd3ad20
LuskGPM <145815917+LuskGPM@users.noreply.github.com>
2026-01-29 00:13:24 -0300

    endpoints adicionado

--- projetoApiWeb/Program.cs
+++ projetoApiWeb/Program.cs
@@ -1,16 +1,11 @@
 using Microsoft.EntityFrameworkCore;
 using Database.service;
 using DbServices.service;
-using TablesDto.Models;
-using Tables.Models;
-using projetoApiWeb.src.Models;
+using projetoApiWeb.src.Controller;
 
 var builder = WebApplication.CreateBuilder(args);
 
-string db_connection_string = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Variavel não encontrada");
-string endpoit_fabricantes = builder.Configuration["ApiSettings:CaminhoFabricante"] ?? "/fabricante";
-
-string connectionString = builder.Configuration.GetConnectionString("lojaDeCarros") ?? db_connection_string;
+string connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Variável não encontrada");
 
 // Builder services
 builder.Services.AddEndpointsApiExplorer();
@@ -23,9 +18,6 @@ builder.Services.AddOpenApiDocument(conf =>
 builder.Services.AddDbContext<DatabaseContext>(opt => opt.UseSqlServer(connectionString));
 builder.Services.AddScoped<CarrosServices>();
 builder.Services.AddScoped<FabricanteServices>();
-builder.Services.Configure<ApiSettings>(
-    builder.Configuration.GetSection("ApiSettings")
-);
 
 // AppMap
 var app = builder.Build();
@@ -44,58 +36,11 @@ if (app.Environment.IsDevelopment())
 
 app.MapGet("/", () => "Hello World!");
 
-// CarrosMap
-var carrosMap = app.MapGroup(endpoit_carros).WithTags("Carros");
-// GETS
-carrosMap.MapGet("/", async (CarrosServices services) => await services.GetAllAsync());
-carrosMap.MapGet("/{Id}", async (CarrosServices services, int Id) => await services.GetByIdAsync(Id));
-// POST
-carrosMap.MapPost("/", async (CarrosServices services, CarroItemDto carro) =>
-{
-    var newCarro = new Carro
-    {
-        Ano = carro.Ano,
-        Cor = carro.Cor,
-        FabricanteId = carro.FabricanteId,
-        Id = carro.Id,
-        Modelo = carro.Modelo
-    };
-    return await services.CreateAsync(newCarro);
-});
-// UPDATE
-carrosMap.MapPatch("/", async (CarrosServices services, CarroItemDto carro) =>
-{
-    var newCarro = new Carro
-    {
-        Ano = carro.Ano,
-        Cor = carro.Cor,
-        FabricanteId = carro.FabricanteId,
-        Id = carro.Id,
-        Modelo = carro.Modelo
-    };
-    return await services.UpdateAsync(newCarro);
-});
-// DELETE
-carrosMap.MapDelete("/{Id}", async (CarrosServices services, int Id) => await services.DeleteAsync(Id));
+// Register Endpoints
+app.MapCarrosEndpoints();
+app.MapFabricantesEndpoints();
 
-// FabricanteMap
-var fabricanteMap = app.MapGroup(endpoit_fabricantes).WithTags("Fabricantes");
-// GET
-fabricanteMap.MapGet("/", async (FabricanteServices services) => await services.GetAllAsync());
-// POST
-fabricanteMap.MapPost("/", async (FabricanteServices services, FabricanteItemDto fabricante) =>
-{
-    var newFabricante = new Fabricante
-    {
-        Id = fabricante.Id,
-        Name = fabricante.Name
-    };
-    return await services.CreateAsync(newFabricante);
-});
-// DELETE
-fabricanteMap.MapDelete("/{Id}", async (FabricanteServices services, int Id) => await services.DeleteAsync(Id));
-
-
+// Migration
 using (var scope = app.Services.CreateScope())
 {
     var services = scope.ServiceProvider;
--- projetoApiWeb/src/Controller/EndPoints.cs
+++ projetoApiWeb/src/Controller/EndPoints.cs
@@ -1,13 +1,12 @@
 using DbServices.service;
-using projetoApiWeb.src.Models;
 using Tables.Models;
 using TablesDto.Models;
 
 namespace projetoApiWeb.src.Controller;
 
-public static class CarroEndpoints
+public static class CarrosEndpoints
 {
-    public static async Task MapCarrosEndpoints(this WebApplication app)
+    public static void MapCarrosEndpoints(this WebApplication app)
     {
         string endpointCarros = app.Configuration["ApiSettings:EndpointCarro"] ??
             throw new InvalidOperationException("Varíavel de ambiente não encontrada");
@@ -15,14 +14,16 @@ public static class CarroEndpoints
         var carrosMap = app.MapGroup(endpointCarros).WithTags("Carros");
 
         carrosMap.MapGet("/", GetAllCarros);
-        carrosMap.MapGet("/{Id}", GetCarrosById);
-        carrosMap.MapPost("/", CreateCarros);
-        carrosMap.MapDelete("/{Id}", );
+        carrosMap.MapGet("/{Id}", GetCarroById);
+        carrosMap.MapPost("/", CreateCarro);
+        carrosMap.MapDelete("/{Id}", DeleteCarroById);
+        carrosMap.MapPatch("/", UpdateCarro);
     }
 
     private static async Task<IResult> GetAllCarros(CarrosServices service) => await service.GetAllAsync();
-    private static async Task<IResult> GetCarrosById(int Id, CarrosServices service) => await service.GetByIdAsync(Id);
-    private static async Task<IResult> CreateCarros(CarroItemDto carro, CarrosServices service)
+    private static async Task<IResult> GetCarroById(int Id, CarrosServices service) => await service.GetByIdAsync(Id);
+    private static async Task<IResult> DeleteCarroById(int Id, CarrosServices service) => await service.DeleteAsync(Id);
+    private static async Task<IResult> CreateCarro(CarroItemDto carro, CarrosServices service)
     {
         var newCarro = new Carro
         {
@@ -34,5 +35,43 @@ public static class CarroEndpoints
         };
         return await service.CreateAsync(newCarro);
     }
-    private static async Task<IResult> DeleteCarroById(int Id, CarrosServices service) => await service.DeleteAsync(Id);
+    private static async Task<IResult> UpdateCarro(CarroItemDto carro, CarrosServices service)
+    {
+        var newCarro = new Carro
+        {
+            Ano = carro.Ano,
+            Cor = carro.Cor,
+            FabricanteId = carro.FabricanteId,
+            Id = carro.Id,
+            Modelo = carro.Modelo
+        };
+        return await service.UpdateAsync(newCarro);
+    }
+}
+
+public static class FabricantesEndpoints
+{
+    public static void MapFabricantesEndpoints(this WebApplication app)
+    {
+        string endpointFabricantes = app.Configuration["ApiSettings:EndpointFabricante"] ??
+            throw new InvalidOperationException("Varíavel de ambiente não encontrada");
+
+        var carrosMap = app.MapGroup(endpointFabricantes).WithTags("Fabricantes");
+
+        carrosMap.MapGet("/", GetAllFabricantes);
+        carrosMap.MapPost("/", CreateFabricante);
+        carrosMap.MapDelete("/{Id}", DeleteFabricanteById);
+    }
+
+    private static async Task<IResult> GetAllFabricantes(FabricanteServices service) => await service.GetAllAsync();
+    private static async Task<IResult> DeleteFabricanteById(int Id, FabricanteServices service) => await service.DeleteAsync(Id);
+    private static async Task<IResult> CreateFabricante(FabricanteItemDto fabricante, FabricanteServices service)
+    {
+        var newFabricante = new Fabricante
+        {
+            Id = fabricante.Id,
+            Name = fabricante.Name
+        };
+        return await service.CreateAsync(newFabricante);
+    }
 }
