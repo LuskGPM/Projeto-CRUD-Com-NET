51358deaa3b04c0557f3ef58ae4bee496e0588d0
LuskGPM <145815917+LuskGPM@users.noreply.github.com>
2026-02-04 19:12:23 -0300

    iniciando mudanças

--- view/src/components/templates/mainComp/FormCadastro.vue
+++ view/src/components/templates/mainComp/FormCadastro.vue
@@ -1,7 +1,7 @@
 <script lang="ts" setup>
 import type { CarroItemDto, FabricanteItemDto } from '@/interfaces/schemas';
 import { useCacheStore } from '@/stores/cache';
-import { inject, ref, type Ref } from 'vue';
+import { computed, inject, ref, type ComputedRef, type Ref } from 'vue';
 
 
 const modeloCarro: Ref<string | null> = ref(null)
@@ -69,6 +69,17 @@ const insertItemCadastroComp = async (ExpectedType: "car" | "fab") => {
     }
 }
 
+const validacaoSelectFabricantes = computed(() => selectedFabricanteForm.value != null)
+const validacaoSelectCor = computed(() => selectedCorForm.value != null)
+const validacaoModeloCarro = computed(() => modeloCarro.value != null && modeloCarro.value.length >= 2)
+
+const validarFormularioCarro = computed(() => {
+    if(selectedCorForm.value && selectedFabricanteForm.value && modeloCarro.value) return false
+    return true
+})
+
+const validacaoFabricante = computed(() => nomeFabricante.value != null && nomeFabricante.value.length > 3)
+
 </script>
 
 <template>
@@ -77,17 +88,17 @@ const insertItemCadastroComp = async (ExpectedType: "car" | "fab") => {
         <BForm v-if="isGerenciamentoDeCarros">
             <BFormFloatingLabel label="Modelo do Veículo" label-for="modeloCarro" class="my-2">
                 <BFormInput id="modeloCarro" type="text" placeholder="Modelo do carro" v-model="modeloCarro"
-                    :disabled="isLoading" />
+                    :disabled="isLoading" :state="validacaoModeloCarro" />
             </BFormFloatingLabel>
 
             <BRow>
                 <BCol>
                     <BFormSelect size="md" v-model="selectedCorForm" :options="optionsCor" class="mt-3"
-                        :disabled="isLoading" />
+                        :disabled="isLoading" :state="validacaoSelectCor" />
                 </BCol>
                 <BCol>
                     <BFormSelect size="md" v-model="selectedFabricanteForm" :options="optionFabricantes" class="mt-3"
-                        :disabled="isLoading">
+                        :disabled="isLoading" :state="validacaoSelectFabricantes">
                         <BFormSelectOption v-for="fabricante in store.getFabricantes" :key="fabricante.id!"
                             :value="fabricante.id">
                             {{ fabricante.name }}
@@ -99,20 +110,21 @@ const insertItemCadastroComp = async (ExpectedType: "car" | "fab") => {
             <label for="anoVeiculo" class="mt-3">Ano do veículo</label>
             <BFormSpinbutton size="lg" id="anoVeiculo" v-model="anoButtonValue" min="1886" :max="anoAtual" class="mb-3"
                 :disabled="isLoading" />
+
             <BButton size="lg" type="button" variant="outline-light" @click="insertItemCadastroComp('car')"
-                :disabled="isLoading">
-                Adicionar
-                <BSpinner v-if="isLoading" />
+                :disabled="validarFormularioCarro || isLoading">
+                <span>{{ isLoading ? "loading..." : "Adicionar" }}</span>
+                <BSpinner v-if="isLoading" small />
             </BButton>
         </BForm>
 
         <BForm v-else>
             <BFormFloatingLabel label="Nome do Fabricante" label-for="nomeFabricante" class="mb-3">
-                <BFormInput type="text" id="nomeFabricante" placeholder="nomeFabricante" v-model="nomeFabricante" />
+                <BFormInput type="text" id="nomeFabricante" placeholder="nomeFabricante" v-model="nomeFabricante" :state="validacaoFabricante" />
             </BFormFloatingLabel>
-            <BButton size="lg" type="button" variant="outline-light" @click="insertItemCadastroComp('fab')">
-                Adicionar
-                <BSpinner v-if="isLoading" />
+            <BButton size="lg" type="button" variant="outline-light" @click="insertItemCadastroComp('fab')" class="d-flex align-items-center justify-content-between" :disabled="isLoading || validacaoFabricante">
+                <span>{{ isLoading ? "loading..." : "Adicionar" }}</span>
+                <BSpinner v-if="isLoading" small/>
             </BButton>
         </BForm>
     </BCard>
--- view/src/components/templates/mainComp/ListItems.vue
+++ view/src/components/templates/mainComp/ListItems.vue
@@ -1,25 +1,37 @@
 <script lang="ts" setup>
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 import { faTrashCan } from '@fortawesome/free-regular-svg-icons';
+import { faPencil } from '@fortawesome/free-solid-svg-icons';
+
 import { useCacheStore } from '@/stores/cache';
 
-import { inject } from 'vue';
+import { inject, ref, type Ref } from 'vue';
+import { BOffcanvas } from 'bootstrap-vue-next';
+import type { CarroItemDto } from '@/interfaces/schemas';
 
 const store = useCacheStore()
 
 const isGerenciamentoDeCarros = inject("isGerCarros")
 
-const deleteItem = async (ExpectedType: "car" | "fab", Id: number) => {
+const deleteItem = async (ExpectedType: "car" | "fab", Id: number): Promise<void> => {
     await store.DeleteItem(ExpectedType, Id)
 }
+
+const showOffCanvas: Ref<boolean, boolean> = ref(false)
+
+const toggleOffCanvas = (carroToEdit: CarroItemDto): void => {
+    showOffCanvas.value = !showOffCanvas.value
+}
+
 </script>
 
 <template>
     <BCard variant="dark" text-variant="light" style="padding: 10px 5px;">
-        <BCardText> Lista de {{ isGerenciamentoDeCarros ? "Carros" : "Fabricantes"}} </BCardText>
+        <BCardText> Lista de {{ isGerenciamentoDeCarros ? "Carros" : "Fabricantes" }} </BCardText>
 
         <BListGroup v-if="!isGerenciamentoDeCarros">
-            <BListGroupItem variant="dark" class="d-flex justify-content-between align-items-center" v-for="fabricante in store.getFabricantes" :key="fabricante.id">
+            <BListGroupItem variant="dark" class="d-flex justify-content-between align-items-center"
+                v-for="fabricante in store.getFabricantes" :key="fabricante.id">
                 <span>{{ fabricante.name }}</span>
                 <BButton @click="deleteItem('fab', fabricante.id)" variant="danger">
                     <FontAwesomeIcon :icon="faTrashCan" />
@@ -28,13 +40,25 @@ const deleteItem = async (ExpectedType: "car" | "fab", Id: number) => {
         </BListGroup>
 
         <BListGroup style="font-size: 1.2em;" v-else>
-            <BListGroupItem variant="dark" class="d-flex justify-content-between align-items-center" v-for="carro in store.paginetedCarros" :key="carro.id">
-                <span>{{ carro.modelo }}</span>
-                <BButton @click="deleteItem('car', carro.id)" variant="danger" >
-                    <FontAwesomeIcon :icon="faTrashCan" />
-                </BButton>
+            <BListGroupItem variant="dark" class="d-flex justify-content-between align-items-center"
+                v-for="carro in store.paginetedCarros" :key="carro.id">
+                <span>{{ carro.modelo }} | {{ carro.ano }}</span>
+                <div class="d-flex" style="gap: 10px;">
+                    <BButton variant="success" @click="toggleOffCanvas(carro)">
+                        <FontAwesomeIcon :icon="faPencil" />
+                    </BButton>
+                    <BButton @click="deleteItem('car', carro.id)" variant="danger">
+                        <FontAwesomeIcon :icon="faTrashCan" />
+                    </BButton>
+                </div>
             </BListGroupItem>
-            <BListGroupItem variant="dark" v-if="store.paginetedCarros.length == 0"> Sem Carros cadastrados </BListGroupItem>
+            <BListGroupItem variant="dark" v-if="store.paginetedCarros.length == 0">
+                Sem Carros cadastrados
+            </BListGroupItem>
         </BListGroup>
     </BCard>
+
+    <BOffcanvas class="m-2" placement="end" v-model="showOffCanvas">
+         
+    </BOffcanvas>
 </template>
